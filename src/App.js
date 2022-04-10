5733983fd7065d384e262e376a7bc3d4744c9b92
cristianomggrandi <cristigrandi14@gmail.com>
2022-04-10 12:33:11 -0300

    Erase useless argument

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <div className="App">
             <Result textToShow={result ? result : current}/>
-            <ButtonGrid number='0' />
+            <ButtonGrid />
         </div>
     );
 }
