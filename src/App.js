ea9a0472e4caf3ccd7ec1d33e403eee3a0725619
cristianomggrandi <cristigrandi14@gmail.com>
2022-04-11 14:19:20 -0300

    Add different functions for each button type

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
 import ButtonGrid from './components/ButtonGrid';
 import Result from './components/Result';
 
 function App() {
 
-    const [result, setResult] = useState();
+    const [result, setResult] = useState(null);
     const [current, setCurrent] = useState(0);
 
-    function handleClick(i) {
-        //switch (typeof i)
-        if ((typeof i) === "number") {
-            setCurrent(i);
-        } else {
-            setCurrent(i);
+    //TODO: insertNumber while result on screen
+
+    function insertNumber(i) {
+        setCurrent(i);
+    }
+
+    function clearValue() {
+        if (current) setCurrent(0);
+        else setResult(null);
+    }
+
+    function equals() {
+        setResult(current);
+        setCurrent(0);
+    }
+
+    function chooseOperator(operator) {
+        setCurrent(operator)
+    }
+
+    function handleClick(i, type) {
+        switch (type) {
+            case 'number':
+                insertNumber(i);
+                break;
+            case 'string':
+                if (Button.operatorOptions.includes(i)) chooseOperator(i);
+                if (i === '=') equals();
+                if (i === 'C') clearValue();
         }
     }
 
     return (
         <div className="App">
             <Result textToShow={result ? result : current} />
-            <ButtonGrid onClick={(i) => handleClick(i)} />
+            <ButtonGrid onClick={(i, type) => handleClick(i, type)} />
         </div>
     );
 }
--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -7,26 +7,26 @@ export default class ButtonGrid extends React.Component {
             <table>
                 <tbody>
                     <tr>
-                        <Button class='button-number' content='0' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='1' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='2' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='3' onClick={(i) => this.props.onClick(i)} />
+                        <Button class='button-number' content='0' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='1' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='2' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='3' onClick={(i, type) => this.props.onClick(i, type)} />
                     </tr>
                     <tr>
-                        <Button class='button-number' content='4' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='5' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='6' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-number' content='7' onClick={(i) => this.props.onClick(i)} />
+                        <Button class='button-number' content='4' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='5' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='6' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-number' content='7' onClick={(i, type) => this.props.onClick(i, type)} />
                     </tr>
                     <tr>
-                        <Button class='button-operator' content='+' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-operator' content='-' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-operator' content='/' onClick={(i) => this.props.onClick(i)} />
-                        <Button class='button-operator' content='x' onClick={(i) => this.props.onClick(i)} />
+                        <Button class='button-operator' content='+' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-operator' content='-' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-operator' content='/' onClick={(i, type) => this.props.onClick(i, type)} />
+                        <Button class='button-operator' content='x' onClick={(i, type) => this.props.onClick(i, type)} />
                     </tr>
                     <tr>
-                        <Button class='button-equals' content='=' onClick={(i) => this.props.onClick(i)} span='3' />
-                        <Button class='button-clear' content='C' onClick={(i) => this.props.onClick(i)} />
+                        <Button class='button-equals' content='=' onClick={(i, type) => this.props.onClick(i, type)} span='3' />
+                        <Button class='button-clear' content='C' onClick={(i, type) => this.props.onClick(i, type)} />
                     </tr>
                 </tbody>
             </table>
